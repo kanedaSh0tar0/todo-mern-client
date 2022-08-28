143e7ed6f31ad8f896a71385ee3094020876e0dd
kanedaSh0tar0 <1nikitos1003@gmail.com>
2022-08-28 22:23:30 +0300

    client deploy 3

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require('path')
-
+const cors = require('./cors')
 const express = require('express')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
+app.use(cors)
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
