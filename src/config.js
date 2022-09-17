7227c64b29d83b7d33d550fa6913938b964a13ca
kanedaSh0tar0 <1nikitos1003@gmail.com>
2022-09-17 19:24:31 +0300

    Change config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const API_URL = 'https://quiet-reaches-83756.herokuapp.com/api/'
-export const API_URL = 'http://localhost:5000/api/'
+export const API_URL = 'https://quiet-reaches-83756.herokuapp.com/api/'
